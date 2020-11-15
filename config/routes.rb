d23bf49ea387fb1015dd5c76c9c47b62b3c0bb1d
JanAwadallah <jan.awadallah@gmail.com>
2020-11-15 17:03:40 +1100

    Semi-final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  post 'message', to: 'messages#create'
+  mount ActionCable.server, at: "/cable"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Jan", password: "password1", email: "jan@gmail.com")
+User.create(username: "Bishoy", password: "password2", email: "bishoy@gmail.com")
+User.create(username: "Lydia", password: "password3", email: "lydia@gmail.com")
+User.create(username: "Maria", password: "password4", email: "maria@gmail.com")
